f96eb7e92b088296d828e294184b65166782440b
Rabindra <rabindra1818@gmail.com>
2023-01-23 15:07:16 +0530

    search and rssfeed image resize

---
+++ imageresize/compressimage.php
@@ -0,0 +1,96 @@
+
+<?php 
+ 
+/* 
+ * Custom function to compress image size and 
+ * upload to the server using PHP 
+ */ 
+function compressImage($source, $destination, $quality) { 
+    // Get image info 
+    $imgInfo = getimagesize($source); 
+    $mime = $imgInfo['mime']; 
+     
+    // Create a new image from file 
+    switch($mime){ 
+        case 'image/jpeg': 
+            $image = imagecreatefromjpeg($source); 
+           imagejpeg($image, $destination, $quality);
+            break; 
+        case 'image/png': 
+            $image = imagecreatefrompng($source); 
+            imagepng($image, $destination, $quality);
+            break; 
+        case 'image/gif': 
+            $image = imagecreatefromgif($source); 
+            imagegif($image, $destination, $quality);
+            break; 
+        default: 
+            $image = imagecreatefromjpeg($source); 
+           imagejpeg($image, $destination, $quality);
+    } 
+     
+     
+    // Return compressed image 
+    return $destination; 
+} 
+ 
+ 
+// File upload path 
+$uploadPath = "photos/"; 
+ 
+// If file upload form is submitted 
+$status = $statusMsg = ''; 
+if(isset($_POST["submit"])){ 
+    $status = 'error'; 
+    if(!empty($_FILES["image"]["name"])) { 
+        // File info 
+        $fileName = basename($_FILES["image"]["name"]); 
+        $imageUploadPath = $uploadPath . $fileName; 
+        $fileType = pathinfo($imageUploadPath, PATHINFO_EXTENSION); 
+         
+        // Allow certain file formats 
+        $allowTypes = array('jpg','png','jpeg','gif'); 
+        if(in_array($fileType, $allowTypes)){ 
+            // Image temp source 
+            $imageTemp = $_FILES["image"]["tmp_name"]; 
+             
+            // Compress size and upload image 
+            $compressedImage = compressImage($imageTemp, $imageUploadPath, 25); 
+             
+            if($compressedImage){ 
+                $status = 'success'; 
+                $statusMsg = "Image compressed successfully."; 
+            }else{ 
+                $statusMsg = "Image compress failed!"; 
+            } 
+        }else{ 
+            $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.'; 
+        } 
+    }else{ 
+        $statusMsg = 'Please select an image file to upload.'; 
+    } 
+} 
+ 
+// Display status message 
+echo $statusMsg; 
+ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resize Image</title>
+</head>
+<body>
+<div style="margin:250px 0; text-align:center; ">
+<form action="" method="post" enctype="multipart/form-data">
+    <label>Select Image File:</label>
+    <input type="file" name="image">
+    <input type="submit" name="submit" value="Upload">
+</form>
+</div>
+    
+</body>
+</html>
